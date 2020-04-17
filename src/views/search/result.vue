7b77b2de588a3033f8f1ef5c658a79f4c8bb2186
sulihua <l1156832448@qq.com>
2020-04-17 11:31:05 +0800

    搜索结果页布局

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class='container'>
-      结果
+  <div class="serach-result">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      title="xxx 的搜索结果"
+      left-arrow
+      fixed
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+
+    <!-- 文章列表 -->
+    <van-list
+      class="article-list"
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell
+        v-for="item in list"
+        :key="item"
+        :title="item"
+      />
+    </van-list>
+    <!-- /文章列表 -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'result',
+  name: 'SearchResult',
   data () {
     return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
 
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
     }
   }
 }
 </script>
 
-<style scoped lang='less'></style>
+<style lang="less" scoped>
+.van-nav-bar {
+  background-color: #3196fa;
+  .van-nav-bar__title {
+    color: #fff;
+  }
+  // 搜索结果页 后退按钮为白色
+  .van-icon{
+    color:#fff;
+  }
+}
+</style>
